684a4aa245287c86e76eb0f577f987f26376135f
victor <victorbravofi@gmail.com>
2019-05-20 20:12:45 -0300

    fix stage again

--- server/src/stage.cpp
+++ server/src/stage.cpp
@@ -12,28 +12,12 @@ Stage::Stage():
 }
 
 void Stage::run() {
-
-    Socket accepterSocket;
-    accepterSocket.bindAndListen("4545");
-    Protocol protocol(accepterSocket.accept());
-
-    protocol.sendQuad(1);
-
     this->keep_running = true;
     while (this->keep_running) {
         this->b2world.Step(
             this->TIME_STEP,
             this->VELOCITY_ITERATIONS,
             this->POSITION_ITERATIONS);
-        b2Body *bodies = this->b2world.GetBodyList();
-        while (bodies != 0) {
-            b2Vec2 pos = bodies->GetPosition();
-            Creator creator(0, 0, pos.x, pos.y);
-            protocol.sendCreator(creator);
-            ++bodies;
-        }
-        break;
-
     }
 }
 
@@ -44,4 +28,18 @@ void Stage::stop() {
 void Stage::createStageOne() {
     /* Solo crea una unica chell en el mundo */
     Chell chell(&this->b2world, 0, 20);
+
+    Socket accepterSocket;
+    accepterSocket.bindAndListen("4545");
+    Protocol protocol(accepterSocket.accept());
+
+    protocol.sendQuad(1);
+
+    b2Body *bodies = this->b2world.GetBodyList();
+    while (bodies != 0) {
+        b2Vec2 pos = bodies->GetPosition();
+        Creator creator(0, 0, pos.x, pos.y);
+        protocol.sendCreator(creator);
+        ++bodies;
+    }
 }
