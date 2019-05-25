5b0aed4eac9ee856e5991feb4ad54d628f11d53c
Lucas <lucas_rack@live.com.ar>
2019-05-25 18:43:26 -0300

    pequeñisimo fix

--- client/src/block.cpp
+++ client/src/block.cpp
@@ -52,8 +52,8 @@ void Block::render(int resx,int resy,int width,int height){
 */
 
 void Block::render(int cx,int cy,int resx,int resy,float scale){
-	int a = this->posx*scale - (this->width/2)*scale + resx/2 ;
-	int b = -this->posy*scale - (this->height/2)*scale + (2*resy)/3;
+	int a = (this->posx-cx)*scale - (this->width/2)*scale + resx/2 ;
+	int b = -(this->posy-cy)*scale - (this->height/2)*scale + (2*resy)/3;
 	this->texturePtr->render(this->frameArea, Area(a,b,this->width*scale,this->height*scale));
 }
 
