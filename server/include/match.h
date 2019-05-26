bd18090286196806127d7723835edf55c2524daa
Victor Bravo <alqace@gmail.com>
2019-05-25 22:07:45 -0300

    inicio match

---
+++ server/include/match.h
@@ -0,0 +1,13 @@
+#ifndef __MATCH_H__
+#define __MATCH_H__
+
+#include "player.h"
+
+/* Representa a una partida de juego */
+class Match {
+private:
+    std::vector<Player *> players;
+
+};
+
+#endif
