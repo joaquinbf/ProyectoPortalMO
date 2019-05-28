1b86ad572ccf6fbdd1caa59da93b56f886802863
victor <victorbravofi@gmail.com>
2019-05-28 18:37:30 -0300

    level 1

--- server/src/match.cpp
+++ server/src/match.cpp
@@ -9,6 +9,17 @@ Match::Match():
 }
 
 void Match::run() {
+    // hardcoded
+    this->world.setB2World(&this->b2world);
+    this->world.setGravity(0.0, -20);
+    this->world.addGround(-100, -50, 100, -50);
+
+    float start = -86.4;
+    float step = 19.2;
+    for (int i = 0; i < 7; i++) {
+        this->world.addSquareStoneBlock(start + step*i, -30);
+    }
+
     while (this->keep_running) {
         this->world.step();
     }
