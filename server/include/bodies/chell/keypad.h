ad65a28eb84d9ab0207b5fe6cbc572794711f9f8
Victor Bravo <alqace@gmail.com>
2019-05-31 23:50:20 -0300

    keypad

--- server/include/bodies/chell/keypad.h
+++ server/include/bodies/chell/keypad.h
@@ -1,8 +1,26 @@
 #ifndef __KEYPAD_H__
 #define __KEYPAD_H__
 
-class Keypad {
+#include "key.h"
+#include <map>
 
+enum KEY {
+    LEFT ,
+    RIGHT,
+    UP,
+    DOWN,
+    FIRE,
+};
+
+class Keypad {
+private:
+    std::map<KEY, Key> keys;
+
+public:
+    Keypad();
+    void press(KEY k);
+    void release(KEY k);
+    double elapsedTimeBeingPressed(KEY k);
 };
 
 #endif
--- server/src/bodies/chell/keypad.cpp
+++ server/src/bodies/chell/keypad.cpp
@@ -1,1 +1,21 @@
 #include "../../../include/bodies/chell/keypad.h"
+
+Keypad::Keypad() {
+    keys[KEY::LEFT] = Key();
+    keys[KEY::RIGHT] = Key();
+    keys[KEY::UP] = Key();
+    keys[KEY::DOWN] = Key();
+    keys[KEY::FIRE] = Key();
+}
+
+void Keypad::press(KEY k) {
+    this->keys[k].press();
+}
+
+void Keypad::release(KEY k) {
+    this->keys[k].release();
+}
+
+double Keypad::elapsedTimeBeingPressed(KEY k) {
+    return this->keys[k].elapsedTimeBeingPressed();
+}
