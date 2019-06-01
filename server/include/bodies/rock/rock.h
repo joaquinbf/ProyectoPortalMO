8d9a8c41e05a539e5eef5c37d87911dde6df03e6
Victor Bravo <alqace@gmail.com>
2019-06-01 03:43:55 -0300

    inicio rock

---
+++ server/include/bodies/rock/rock.h
@@ -0,0 +1,30 @@
+#ifndef __ROCK_H__
+#define __ROCK_H__
+
+#include "../body.h"
+#include "../../../../libs/Box2D-master/Box2D/Dynamics/b2World.h"
+#include "../../../../libs/Box2D-master/Box2D/Dynamics/b2Body.h"
+#include "../../../../libs/Box2D-master/Box2D/Dynamics/b2Fixture.h"
+#include "../../../../libs/Box2D-master/Box2D/Collision/Shapes/b2PolygonShape.h"
+#include <cstdint>
+
+class Rock: public Body {
+private:
+    const float HALF_WIDTH = 10;
+    const float HALF_HEIGHT = 15;
+    const float DENSITY = 1;
+    const float ANGLE = 0;
+
+public:
+    /* Instancia una roca de id 'body_id' sobre b2orld en la
+     * posicion (x, y) */
+    Rock(uint32_t body_id, b2World *b2world, float x, float y);
+
+    /* Libera los recursos utilizados */
+    ~Rock();
+
+    /* Devuelve una update de la roca */
+    virtual Update createUpdate(COMMAND command) const;
+};
+
+#endif
---
+++ server/src/bodies/rock/rock.cpp
@@ -0,0 +1,43 @@
+#include "../../../include/bodies/rock/rock.h"
+
+#include "../../../../libs/Box2D-master/Box2D/Dynamics/b2World.h"
+#include "../../../../libs/Box2D-master/Box2D/Dynamics/b2Body.h"
+#include "../../../../libs/Box2D-master/Box2D/Dynamics/b2Fixture.h"
+#include "../../../../libs/Box2D-master/Box2D/Collision/Shapes/b2PolygonShape.h"
+#include <cstdint>
+
+Rock::Rock(uint32_t body_id, b2World *b2world, float x, float y):
+    Body(body_id) {
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(x, y);
+    bodyDef.angle = this->ANGLE;
+    bodyDef.userData = (void *) this;
+    bodyDef.fixedRotation = true;
+
+    this->b2body = b2world->CreateBody(&bodyDef);
+
+    b2PolygonShape boxShape;
+    boxShape.SetAsBox(this->HALF_WIDTH, this->HALF_HEIGHT);
+
+    b2FixtureDef boxFixtureDef;
+    boxFixtureDef.shape = &boxShape;
+    boxFixtureDef.density = this->DENSITY;
+
+    this->b2body->CreateFixture(&boxFixtureDef);
+}
+
+Rock::~Rock() {
+}
+
+Update Rock::createUpdate(COMMAND command) const {
+    Update update(
+        command,
+        ENTITY::ROCK,
+        this->getBodyId(),
+        STATUS::NONE_STATUS,
+        this->b2body->GetPosition().x,
+        this->b2body->GetPosition().y,
+        0);
+    return update;
+}
