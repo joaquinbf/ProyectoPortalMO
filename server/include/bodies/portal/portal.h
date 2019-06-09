d772bc34b3db2f250117b350577a358398d26cf1
Victor Bravo <alqace@gmail.com>
2019-06-09 19:56:27 -0300

    inicio portal

---
+++ server/include/bodies/portal/portal.h
@@ -0,0 +1,10 @@
+#ifndef __PORTAL_H__
+#define __PORTAL_H__
+
+#include "../body.h"
+
+class Portal: public Body {
+
+};
+
+#endif
---
+++ server/src/bodies/portal/portal.cpp
@@ -0,0 +1,1 @@
+#include "../../../include/bodies/portal/portal.h"
