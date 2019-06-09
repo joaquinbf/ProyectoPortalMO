09ad3c1d8eaba42244b3d30b3d2933b3a1ebbd39
Victor Bravo <alqace@gmail.com>
2019-06-09 14:30:42 -0300

    inicio receiver

---
+++ server/include/bodies/receiver/receiver.h
@@ -0,0 +1,10 @@
+#ifndef __RECEIVER_H__
+#define __RECEIVER_H__
+
+#include "../body.h"
+
+class Receiver: public Body {
+
+};
+
+#endif
---
+++ server/src/bodies/receiver/receiver.cpp
@@ -0,0 +1,1 @@
+#include "../../../include/bodies/receiver/receiver.h"
