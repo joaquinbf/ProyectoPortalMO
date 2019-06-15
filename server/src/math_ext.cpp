0c8d1cef11514df1b3cbcd7788921837b4d6c706
Victor Bravo <alqace@gmail.com>
2019-06-15 00:57:43 -0300

    math ext

---
+++ server/src/math_ext.cpp
@@ -0,0 +1,10 @@
+#include "../include/math_ext.h"
+
+b2Vec2 MathExt::reflect(b2Vec2 n, b2Vec2 v) {
+    b2Vec2 r = v - (1/n.LengthSquared())*(2*(v*n)*n);
+    return r;
+}
+
+float operator*(const b2Vec2& v, const b2Vec2& u) {
+    return (v.x * u.x) + (v.y + u.y);
+}
