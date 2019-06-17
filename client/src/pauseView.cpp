db741584c37226f74a1dd9337bb1e54aec2ea4dc
Lucas <lucas_rack@live.com.ar>
2019-06-17 20:54:58 -0300

    inicio pause widget

--- client/src/pauseView.cpp
+++ client/src/pauseView.cpp
@@ -1,40 +1,22 @@
 #include "../include/pauseView.h"
 #include <iostream>
 PauseView::PauseView(const TextureManager& tm,const SdlWindow& window):
-textureManager(tm),window(window),blackArea(0,0,600,600){
+textureManager(tm),blackArea(0,0,600,600),pwPause(window,"Pausa",0.05,0.05),
+pwResume(window,"Resumir",0.5,0.333),pwOptions(window,"Opciones",0.5,0.5),
+pwExit(window,"Salir",0.5,0.666){
 	this->blackTexture = this->textureManager.getBlackTexturePointer();
 	this->blackTexture->setAlpha(160);	
-	if( TTF_Init() == -1 ) { 
-		printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() ); 
-	}
-	this->font = TTF_OpenFont( "../resources/fonts/Roboto-Black.ttf", 28 );
-	if(this->font == NULL){
-		std::cout<<"NULL\n";
-	}
-	this->textColor = { 255, 255, 255 };
 }
 
-PauseView::~PauseView(){
-	TTF_CloseFont( this->font );
-}
+PauseView::~PauseView(){}
      		
 
 void PauseView::render(uint32_t resx,uint32_t resy){
 	this->blackTexture->render(this->blackArea,Area(0,0,resx,resy));	
-	std::string str = "PAUSA";
-	this->textSurface = TTF_RenderText_Solid( this->font, str.c_str(), this->textColor );
-	this->mTexture = SDL_CreateTextureFromSurface( this->window.getRenderer(), this->textSurface );
-	Area src(0,0,textSurface->w,textSurface->h);
-	Area dest(10,10,200,100);
-	SDL_Rect sdlSrc = {
-            src.getX(), src.getY(),
-            src.getWidth(), src.getHeight()
-    };
-    SDL_Rect sdlDest = {
-            dest.getX(), dest.getY(),
-            dest.getWidth(), dest.getHeight()
-    };
-    SDL_RenderCopy(this->window.getRenderer(), mTexture, &sdlSrc, &sdlDest);    
+	pwPause.render(resx,resy);
+	pwResume.render(resx,resy);
+	pwOptions.render(resx,resy);
+	pwExit.render(resx,resy);
 }
 
 void PauseView::mouseButtonDown(uint32_t x,uint32_t y){
--- pantalla_inicial/cmake.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir build
-cd build/
-cmake ..
-make
-./portal_init
-cd ..
-rm -r build/
