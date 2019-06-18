9aad700f538adc7dbe641f5a39d6d4cda8b9a282
victor <victorbravofi@gmail.com>
2019-06-18 19:36:20 -0300

    dev: chell jig

---
+++ server/include/bodies/chell/jiging_state.h
@@ -0,0 +1,14 @@
+#ifndef __JIGING_STATE_H__
+#define __JIGING_STATE_H__
+
+#include "chell_state.h"
+
+class Chell;
+
+class JigingState: public ChellState {
+public:
+    JigingState(Chell *chell);
+};
+
+
+#endif
---
+++ server/src/bodies/chell/jiging_state.cpp
@@ -0,0 +1,5 @@
+#include "../../../include/bodies/chell/jiging_state.h"
+
+JigingState::JigingState(Chell *chell):
+    ChellState(chell, STATUS::CHELL_JIGING) {
+}
