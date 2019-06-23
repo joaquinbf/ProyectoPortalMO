105608df80442edd4f7c758026b054a2b78ccd92
Victor Bravo <alqace@gmail.com>
2019-06-23 17:09:59 -0300

    se eliminan todos los tests

--- test/server/world_test.h
+++ test/server/world_test.h
@@ -2,11 +2,12 @@
 #define __WORLD_TEST_H__
 
 #include <cxxtest/TestSuite.h>
-#include "../../server/include/world.h"
 
 class WorldTest: public CxxTest::TestSuite {
 public:
-
+    void testAddition() {
+        TS_ASSERT_EQUALS(5, 2 + 3);
+    }
 };
 
 #endif
